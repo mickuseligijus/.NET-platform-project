b7818d1a8b6adf460325faa3553bc00d0b8a612e
Eligijus Mickus <mickus.eligijus@gmail.com>
2022-10-24 20:48:18 +0300

    setting up database

---
+++ BeTraveling/BeTraveling/Context/BeTravelingDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BeTraveling.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeTraveling.Context
+{
+    public class BeTravelingDbContext : DbContext
+    {
+
+        public BeTravelingDbContext(DbContextOptions<BeTravelingDbContext> options) : base(options)    
+        {
+                
+        }
+        public DbSet<User> Users { get; set; }
+        
+    }
+}
--- BeTraveling/BeTraveling/Program.cs
+++ BeTraveling/BeTraveling/Program.cs
@@ -1,3 +1,5 @@
+using BeTraveling.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BeTravelingDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("dbconnection")));
 
 var app = builder.Build();
 
