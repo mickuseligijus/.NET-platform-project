ec917b941e9ae09ff76eae8c64a5f415822b9cb4
Eligijus Mickus <mickus.eligijus@gmail.com>
2022-10-31 16:28:29 +0200

    adding PostController class

---
+++ BeTraveling/BeTraveling/Controllers/PostController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeTraveling.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+    }
+}
