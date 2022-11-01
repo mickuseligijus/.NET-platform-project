15dc7fc97faf64bd05ee8b77d9f4d2a5b52907b7
Eligijus Mickus <mickus.eligijus@gmail.com>
2022-11-01 08:11:48 +0200

    get my post list

--- BeTraveling/BeTraveling/Controllers/PostController.cs
+++ BeTraveling/BeTraveling/Controllers/PostController.cs
@@ -1,7 +1,10 @@
 ﻿using BeTraveling.Context;
 using BeTraveling.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Configuration;
 using System.Security.Claims;
+using System.Text.RegularExpressions;
 
 namespace BeTraveling.Controllers
 {
@@ -18,14 +21,42 @@ namespace BeTraveling.Controllers
 
 
         [HttpPost]
-        [Route("share")]
+        [Authorize]
+        [Route("post")]
         public async Task<IActionResult> SharePost([FromBody] Post post)
         {
             var currentUser = GetCurrentUser();
-            return Ok();
+            post.UserId = currentUser.Id;
+            post.Created = DateTime.Now;
+            _context.Add(post);
+            await _context.SaveChangesAsync();
+            return Ok("Post was posted successfully");
         }
 
+        [HttpGet]
+        [Authorize]
+        [Route("myposts")]
+        public IActionResult GetMyPosts()
+        {
+            var currentUser = GetCurrentUser();
+            var posts = _context.Posts.Where(post => post.UserId == currentUser.Id).ToList();
 
+            var accumulator = new List<object>()
+            .Select(t => new {Post = default(Post), ReactionNumber = default(int), CommentsNumber = default(int) }).ToList();
+
+            if(posts != null)
+            foreach (var post in posts)
+            {
+                var reactionsNo = _context.PostReactions.Where(reaction => post.Id == reaction.PostId).Count();
+                var commentsNo = _context.Comments.Where(comment => post.Id == comment.PostId).Count();
+
+                var result = new {Post=post,  ReactionNumber = reactionsNo, CommentsNumber = commentsNo};
+                accumulator.Add(result);
+
+            }
+
+            return Ok(accumulator);
+        }
         private User GetCurrentUser()
         {
             var identity = HttpContext.User.Identity as ClaimsIdentity;
